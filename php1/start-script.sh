c6afb2717cb489a6686dccc53c840facd062d26b
pierre265 <kevinngako@gmail.com>
2024-05-09 10:45:44 +0200

    Script Filles

---
+++ php1/start-script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Installation des dépendances Composer
+composer install
+# Génération de la clé d'application Laravel
+php artisan key:generate
+# Migration et seeding de la base de données
+php artisan migrate:fresh --seed
+# Démarrage du serveur PHP
+php-fpm
+
